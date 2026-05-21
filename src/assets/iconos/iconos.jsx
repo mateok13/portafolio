4c5aa5717eda3c4361ab745112b0b4fa26316b3b
mateok13 <mateoroserok13@gmail.com>
2026-05-20 22:20:50 -0500

    clean

--- src/assets/iconos/iconos.jsx
+++ src/assets/iconos/iconos.jsx
@@ -1,24 +1,6 @@
-import AngularIcon from "./angular.png";
-import ReactIcon from "./react.png";
-import HTMLIcon from "./html.png";
-import CSSIcon from "./css.png";
-import JavaScriptIcon from "./js.png";
-import VerIcon from "./ver.png";
-import MusicaIcon from "./escuchar.png";
-import CocinarIcon from "./cocinar.png";
-import FutbolIcon from "./futbol.png";
 import FlechaSubir from "./flechaSubir.png";
 
 
 export default {
-    "AngularIcon": AngularIcon,
-    "ReactIcon": ReactIcon,
-    "HTMLIcon": HTMLIcon,
-    "CSSIcon": CSSIcon,
-    "JavaScriptIcon": JavaScriptIcon,
-    "VerIcon": VerIcon,
-    "MusicaIcon": MusicaIcon,
-    "CocinarIcon": CocinarIcon,
-    "FutbolIcon": FutbolIcon,
     "FlechaSubir": FlechaSubir
 }
--- src/assets/recursos.jsx
+++ src/assets/recursos.jsx
@@ -1,10 +1,9 @@
 import Curriculum from "./Curriculum.pdf";
 import Foto from "./foto.jpg";
 import GitHub from "./github.png";
-import Carta from "./carta.jpg"
 import Alura from "./aluraoracle.png";
 import Unicauca from "./unicauca.png"
 
 export {
-    Curriculum, Foto, Carta, Alura, Unicauca, GitHub
+    Curriculum, Foto, Alura, Unicauca, GitHub
 }
